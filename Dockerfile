4aa298a577ac9cda4dee3d4b67e4e3a256658609
Nic Townsend <nictownsend@uk.ibm.com>
2022-07-08 12:19:03 +0100

    kustomize

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,4 @@ RUN mkdir /temp
 WORKDIR /opt/ibm
 COPY target/kafka-sample-runner.jar .
 RUN chgrp -R 0 /temp && chmod -R g+rwX /temp
-CMD java -Djava.io.tmpdir=/temp -jar kafka-sample-runner.jar > /opt/ibm/files/messages.json
+CMD java -Djava.io.tmpdir=/temp -jar kafka-sample-runner.jar > /opt/ibm/files/input/messages.json
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
-TAG="${1:-1.0}"
-#TODO template in the repo and image tag to the yaml
+REPO="${1:-missing-repo-param}"
+TAG="${2:-1.0}"
+NAMESPACE="${3:-missing-namespace-param}"
 mvn package
-docker build -t "localhost:5000/kafka-sample-producer:${TAG}" .
-docker tag "localhost:5000/kafka-sample-producer:${TAG}" "quay.io/nictownsend/kafka-sample-producer:${TAG}"
-docker push "quay.io/nictownsend/kafka-sample-producer:${TAG}"
-oc delete -f deployment.yml
-oc delete kt txns txn_count
-oc apply -f deployment.yml
+
+IMAGE_NAME=flink-workload-generator
+IMAGE="${REPO}/${IMAGE_NAME}"
+
+docker build -t "${IMAGE_NAME}:${TAG}" .
+docker tag "${IMAGE_NAME}:${TAG}" "${IMAGE}:${TAG}"
+docker push "${IMAGE}:${TAG}"
+
+cd deploy
+cp kustomization.template.yml kustomization.yml
+yq -i "
+  .namespace = ${NAMESPACE} |
+  .images += {"newName": "${IMAGE}", "newTag" : "${TAG}"}
+" kustomization.yaml
+
+oc apply -k .
+cd - || exit
